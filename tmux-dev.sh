ff61d529e09bdf4fc5bc601e0abddb4ec78f8a58
tobinbradley <tobin.bradley@gmail.com>
2017-10-05 14:58:48 -0400

    made tmux dev screen have split bottom panes

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -6,6 +6,7 @@ if [ $? != 0 ]
 then
      tmux new-session -s $session -n editor -d
      tmux split-window -t $session -v -p 20
+     tmux split-window -t $session -h -p 50
      tmux attach -t $session
 else
      tmux attach -t $session
